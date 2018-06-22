47f3c43e85414dfc768db7605dfee66a155f8317
Rogelio Ventura <rogeliovr94@gmail.com>
2018-06-22 14:58:15 -0500

    Updated <li> tag gug

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,3 +1,7 @@
-const parseHtml = message => message.replace(/<li>/g, '\n<strong>').replace(/<\/li>/g, '\n</strong>').replace(/<b>|<strong>/g, '||b¬¬').replace(/<\/b>|<\/strong>/g, '||/b¬¬').replace(/<a/, '||a').replace(/<\/a/g, '||/a').replace(/<p>/g, '\n').replace(/">/g, '¬¬').replace(/<(?:.|\n)*?>/gm, '').replace(/\|\|/g, '<').replace(/¬¬/g, '>').replace(/ class="a-popup/g, '');
+'use strict';
+
+var parseHtml = function parseHtml(message) {
+  return message.replace(/<li>/g, '\n<strong>*</strong>').replace(/<b>|<strong>/g, '||b¬¬').replace(/<\/b>|<\/strong>/g, '||/b¬¬').replace(/<a/, '||a').replace(/<\/a/g, '||/a').replace(/<p>/g, '\n').replace(/">/g, '¬¬').replace(/<(?:.|\n)*?>/gm, '').replace(/\|\|/g, '<').replace(/¬¬/g, '>').replace(/ class="a-popup/g, '');
+};
 
 module.exports.parseHtml = parseHtml;
