9289e300d3470710ef77bfb31155ff9d9ec50841
Rogelio Ventura <rogeliovr94@gmail.com>
2018-06-25 17:24:28 -0500

    updated parameters

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var parseHtml = function parseHtml(message) {
-  return message.replace(/<li>/g, '\n||b¬¬*||/b¬¬').replace(/<strong>/g, '||b¬¬').replace(/<\/strong>/g, '||/b¬¬').replace(/<p>/g, '').replace(/<\/p>/g, '\n').replace(/<br>/g, '\n').replace(/<a/g, '||a').replace(/<\/a>/g, '||/a¬¬').replace().replace(/<(?:.|\n)*?>/gm, '').replace(/ class="a-popup"/g, '').replace(/\|\|/g, '<').replace(/¬¬/g, '>').replace(/\/>/g, '/">');
+  return message.replace(/<li>/g, '\n||b¬¬*||/b¬¬').replace(/<strong>|<b>/g, '||b¬¬').replace(/<\/b>/g, '||/b¬¬').replace(/<\/strong>/g, '||/b¬¬').replace(/<p>/g, '').replace(/<\/p>/g, '\n').replace(/<br>/g, '\n').replace(/<a/g, '||a').replace(/<\/a>/g, '||/a¬¬').replace().replace(/<(?:.|\n)*?>/gm, '').replace(/ class="a-popup"| class='a-popup'| target=_blank/g, '').replace(/\|\|/g, '<').replace(/¬¬/g, '>').replace(/\/>/g, '/">');
 };
 
 module.exports.parseHtml = parseHtml;
