72bdf672a253c6f253853f04a0f4955add4dff8d
Rogelio Ventura <rogeliovr94@gmail.com>
2018-06-26 17:19:44 -0500

    updated function to remove unnecessary attibutes in tags for telegram

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -1,7 +1,16 @@
 'use strict';
 
 var parseHtml = function parseHtml(message) {
-  return message.replace(/<li>/g, '\n||b¬¬*||/b¬¬').replace(/<strong>|<b>/g, '||b¬¬').replace(/<\/b>/g, '||/b¬¬').replace(/<\/strong>/g, '||/b¬¬').replace(/<p>/g, '').replace(/<\/p>/g, '\n').replace(/<br>/g, '\n').replace(/<a/g, '||a').replace(/<\/a>/g, '||/a¬¬').replace().replace(/<(?:.|\n)*?>/gm, '').replace(/ class="a-popup"| class='a-popup'| target=_blank/g, '').replace(/\|\|/g, '<').replace(/¬¬/g, '>').replace(/\/>/g, '/">');
+	return message.replace(/<li>/g, '\n||b¬¬*||/b¬¬').replace(/<strong>|<b>/g, '||b¬¬').replace(/<\/b>/g, '||/b¬¬').replace(/<\/strong>/g, '||/b¬¬').replace(/<\/p>/g, '\n').replace(/<br>/g, '\n').replace(/<a/g, '||a').replace(/<\/a>/g, '||/a¬¬').replace(/<(?:.|\n)*?>/gm, '').replace(/\|\|/g, '<').replace(/¬¬/g, '>').replace(/\/>/g, '/">');
 };
 
-module.exports.parseHtml = parseHtml;
+var htmlToTelegram = function htmlToTelegram(message) {
+	var args = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+
+	var arrToRegEx = args.toString().replace(/,/g, '|');
+	var reg = new RegExp(arrToRegEx, "g");
+	message = parseHtml(message);
+	return message.replace(reg, '');
+};
+
+module.exports.htmlToTelegram = htmlToTelegram;
